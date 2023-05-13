5c20d4b448edf915e97f64cf7c7ab47b94f7790b
louis <Louiis1997@hotmail.fr>
2023-05-13 12:23:51 +0200

    QUESTION 1 ET 2

--- app/projet.py
+++ app/projet.py
@@ -1,37 +1,34 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import F
+from pyspark.sql.functions import sum, count
 from time import sleep
 
 spark = SparkSession \
     .builder \
     .appName("spark-ui") \
-    .master("spark://spark-master:7077") \
-    .config("spark.executor.memory", "4g") \
-    .config("spark.executor.cores", 6) \
+    .master("local[*]") \
     .getOrCreate()
 
-full_file = "/app/data/full.csv"
+full_file = "app/data/full.csv"
 
-# 1
 df = spark.read \
     .option("header", "true") \
     .option("inferSchema", "true") \
-    .load(full_file, format="csv") \
-    .repartition(10)
+    .option("multiline", "true") \
+    .load(full_file, format="csv")
 
-# # 2
-mnm_df = mnm_df \
-    .groupBy("State") \
-    .agg(sum("Count").alias("Total")) \
-    .orderBy("Total", ascending=False) \
+# 1 - Les 10 projets Github pour lesquels il y a eu le plus de commit
+top_projects = df.groupBy("repo").count() \
+                 .orderBy("count", ascending=False).limit(10)
+print("Les 10 projets Github pour lesquels il y a eu le plus de commit :")
+top_projects.show()
 
-mnm_df.show(n=10, truncate=False)
-
-# 5
-mnm_df.cache()
-
-for i in range(10):
-    print(mnm_df.count())
+# 2 - Le plus gros contributeur (la personne qui a fait le plus de commit) du projet apache/spark
+spark_contributors = df.filter("repo = 'apache/spark'") \
+                      .groupBy("author").count() \
+                      .orderBy("count", ascending=False)
+top_contributor = spark_contributors.first()
+print("Le plus gros contributeur du projet apache/spark :")
+print(f"{top_contributor['author']}: {top_contributor['count']} commits")
 
 sleep(1000)
 
